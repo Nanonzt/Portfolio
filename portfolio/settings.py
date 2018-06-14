5b2fd8ba72c5d8cddcef2f92cf1a2a05c6ce2251
Nano <Nano.nzt@gmail.com>
2018-06-14 18:21:30 -0500

    Added Static Files and Minor Changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb2',
-        'USER':'Tyler',
-        'PASSWORD':'django1234',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'NAME': '',
+        'USER':'',
+        'PASSWORD':'',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
